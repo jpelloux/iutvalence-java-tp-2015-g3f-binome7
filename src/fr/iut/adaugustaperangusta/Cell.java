e0590b43dfd6d4d1e74a76da1f2c222dd593f8d1
Axce <axel.ceard@iut-valence.fr>
2015-03-31 22:36:29 +0200

    Réalisation des constructeurs de cell
    il faut suppr voidOverlay et VoidTraveller et les remplacer par null

--- src/fr/iut/adaugustaperangusta/Cell.java
+++ src/fr/iut/adaugustaperangusta/Cell.java
@@ -4,13 +4,14 @@ package fr.iut.adaugustaperangusta;
 import fr.iut.adaugustaperangusta.overlay.Overlay;
 import fr.iut.adaugustaperangusta.overlay.VoidOverlay;
 import fr.iut.adaugustaperangusta.traveller.Traveller;
+import fr.iut.adaugustaperangusta.traveller.VoidTraveller;
 
 /* TODO JAVADOC. */
 public class Cell
 {
-	/* Une Cell (case abstraite) contient :
-	 * 0 ou 1 Overlay (case concrète de la map (sol, mur, target))
-	 * 0 ou 1 Traveller (Character ou Block)
+	/*
+	 * Une Cell (case abstraite) contient : 0 ou 1 Overlay (case concrète de la
+	 * map (sol, mur, target)) 0 ou 1 Traveller (Character ou Block)
 	 */
 	/* TODO JAVADOC. */
 	private Overlay		cellOverlay;
@@ -20,16 +21,35 @@ public class Cell
 	public Cell()
 	{
 		this.cellOverlay = new VoidOverlay();
-		//TODO supprimer VoidOverlay et VoidTraveller et les remplacer par null
+		this.cellTraveller = new VoidTraveller();
 	}
 	
 	/* TODO JAVADOC. */
-	public Cell(Overlay content)
+	public Cell(Overlay over)
 	{
-		this.cellOverlay = content;
+		this.cellOverlay = over;
+		this.cellTraveller = new VoidTraveller();
 	}
 	
-	// TODO Meilleurs constructeurs, pour mettre un Traveller sur la Cell
+	public Cell(Traveller trav)
+	{
+		this.cellTraveller = trav;
+		this.cellOverlay = new VoidOverlay();
+	}
+	
+	public Cell(Traveller trav, Overlay over)
+	{
+		this.cellTraveller = trav;
+		this.cellOverlay = over;
+	}
+	
+	public Cell(Overlay over, Traveller trav)
+	{
+		this(trav, over);
+	}
+	
+	// TODO supprimer VoidOverlay et VoidTraveller et les remplacer par null
+
 	
 	/* TODO JAVADOC. */
 	private boolean isAccessible()
