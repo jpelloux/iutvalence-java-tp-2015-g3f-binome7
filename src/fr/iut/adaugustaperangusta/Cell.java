8f823aaba1adbdade86e2b1f1dec34c5af9218d1
Axce <axel.ceard@iut-valence.fr>
2015-04-04 13:30:56 +0200

    Javadoc Cell

--- src/fr/iut/adaugustaperangusta/Cell.java
+++ src/fr/iut/adaugustaperangusta/Cell.java
@@ -3,56 +3,92 @@ package fr.iut.adaugustaperangusta;
 import fr.iut.adaugustaperangusta.overlay.*;
 import fr.iut.adaugustaperangusta.traveller.*;
 
-/* TODO JAVADOC. */
+/**
+ * Case divisant la Map.
+ * Elle contient 0 ou 1 Overlay.
+ * Elle contient 0 ou 1 Traveller.
+ */
 public class Cell
 {
-	/*
-	 * Une Cell (case abstraite) contient : 0 ou 1 Overlay (case concrète de la
-	 * map (sol, mur, target)) 0 ou 1 Traveller (Character ou Block)
+	/**
+	 * Overlay présent sur la Cell.
+	 * Peut valoir null.
 	 */
-	/* TODO JAVADOC. */
 	private Overlay		cellOverlay;
+	
+	/**
+	 * Traveller présent sur la Cell.
+	 * Peut valoir null.
+	 */
 	private Traveller	cellTraveller;
-
-	/* TODO JAVADOC. */
+	
+	/**
+	 * Constructeur Cell.
+	 * 
+	 * @param over L'Overlay présent sur la Cell. Peut valoir null.
+	 * @param trav Le Traveller présent sur la Cell. Peut valoir null.
+	 */
 	public Cell(Overlay over, Traveller trav)
 	{
 		this.cellOverlay = over;
 		this.cellTraveller = trav;
 	}
-
+	
+	/**
+	 * Constructeur vide Cell.
+	 * Il crée une Cell vide, sans Overlay ni Traveller.
+	 */
 	public Cell()
 	{
 		this(null, null);
 	}
-
+	
+	/**
+	 * Constructeur Cell sans Traveller.
+	 * Il crée une Cell contenant un Overlay.
+	 */
 	public Cell(Overlay over)
 	{
 		this(over, null);
 	}
-
+	
+	/**
+	 * Constructeur Cell sans Overlay.
+	 * Il crée une Cell contenant un Traveller.
+	 */
 	public Cell(Traveller trav)
 	{
 		this(null, trav);
 	}
 	
-	/* TODO JAVADOC. */
+	/**
+	 * Test de praticabilité.
+	 * Doit être appelée avant le déplacement d'un Character.
+	 * 
+	 * @return true si la Cell est praticable, false sinon.
+	 */
 	private boolean isAccessible()
 	{
-		/* TODO Pour votre culture il est à proscrire les instanceof en programmation objet, il faut les utiliser le
-			moins souvent possible.
-		 */
+		// TODO tenter une autre technique que instanceof.
 		if (this.cellOverlay == null || this.cellOverlay instanceof Wall
 				|| this.cellTraveller != null)
 			return false;
 		return true;
+		
 		// TODO isAccessible : rendre accessible les Cell contenant un Block
 	}
 	
-	/* TODO JAVADOC. */
+	/**
+	 * Méthode d'affichage.
+	 * Renvoie trois caractère, sous la forme "Overlay Traveller Overlay".
+	 * Un Overlay null est représenté par le caractère "/".
+	 * Un Traveller null est représenté par un espace.
+	 */
 	public String toString()
 	{
+		// TODO utiliser un StringBuffer/StringBuilder
 		String str = new String();
+		
 		// TODO À simplifier avec des ternaires.
 		if (this.cellOverlay == null)
 			str += "/";
