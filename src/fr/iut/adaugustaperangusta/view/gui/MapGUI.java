17fd9647dd843c52a6e14c72feed83854dc077a3
pellouju <pellouju@PC-B103-01.iut-valence.fr>
2015-05-22 09:27:02 +0200

    try img

--- src/fr/iut/adaugustaperangusta/view/gui/MapGUI.java
+++ src/fr/iut/adaugustaperangusta/view/gui/MapGUI.java
@@ -1,8 +1,11 @@
 package fr.iut.adaugustaperangusta.view.gui;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MapGUI extends JPanel
@@ -13,6 +16,9 @@ public class MapGUI extends JPanel
 		for (int i=0 ; i<25 ; i++)
 		{
 			this.add(new JButton(Integer.toString(i)));
+		//JLabel image = new JLabel( new ImageIcon( "no.jpg"));
+//			this.setLayout(new BorderLayout());
+			//this.add(image);
 		}
 	}
 }
