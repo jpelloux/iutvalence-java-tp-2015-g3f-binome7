d887ef0bce200e08838279e1eb370b2598578d7b
pellouju <pellouju@PC-B103-08.iut-valence.fr>
2015-05-26 09:29:18 +0200

    GUI

--- src/fr/iut/adaugustaperangusta/view/gui/MainWindow.java
+++ src/fr/iut/adaugustaperangusta/view/gui/MainWindow.java
@@ -13,17 +13,23 @@ import javax.swing.JPanel;
 public class MainWindow extends JFrame
 {
 	
+	private static final int DEFAULT_MENU_BAR_SIZE = 40;
+	private static final int DEFAULT_MAP_SIZE = 250;
+
 	public MainWindow()
 	{
 	    this.setTitle("Ad Augusta Per Angusta");
-	  //  this.setSize(400, 100);
+	   
+	    this.setSize(DEFAULT_MAP_SIZE,DEFAULT_MAP_SIZE+DEFAULT_MENU_BAR_SIZE);
+	    this.setResizable(true);
 	    this.setLocationRelativeTo(null);
+	    
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    
 	    this.setJMenuBar(new MenuBar());
 	    
 	    this.add(new MapGUI());
-	    
+
 	    this.setVisible(true);
 	}
 }
--- src/fr/iut/adaugustaperangusta/view/gui/MapGUI.java
+++ src/fr/iut/adaugustaperangusta/view/gui/MapGUI.java
@@ -1,8 +1,13 @@
 package fr.iut.adaugustaperangusta.view.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -10,15 +15,33 @@ import javax.swing.JPanel;
 
 public class MapGUI extends JPanel
 {
+	/**
+	 * 
+	 */
 	public MapGUI()
 	{
 		this.setLayout(new GridLayout(5,5));
+
 		for (int i=0 ; i<25 ; i++)
 		{
-			this.add(new JButton(Integer.toString(i)));
-		//JLabel image = new JLabel( new ImageIcon( "no.jpg"));
-//			this.setLayout(new BorderLayout());
-			//this.add(image);
+			//this.add(new JButton(Integer.toString(i)));
+			
+			JLabel image = new JLabel( new ImageIcon( "cell.png"));
+			this.add(image);
+
 		}
 	}
+	  //public void paintComponent(Graphics g){
+		 // for (int i=0 ; i<25 ; i++)
+		 // {
+			//  try {
+			//	  Image img = ImageIO.read(new File("cell.png"));
+			//	  g.drawImage(img, i%5*50, i/5*50, this);
+		      //Pour une image de fond
+		      //g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+			//  } catch (IOException e) {
+		    //  e.printStackTrace();
+		  //  }                
+		 // }  
+	//  }
 }
