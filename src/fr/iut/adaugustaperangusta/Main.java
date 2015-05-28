b8bbbbaf2e121c04960012be1a50e85f08bef490
pellouju <pellouju@PC-B102-04.iut-valence.fr>
2015-05-28 08:37:54 +0200

    Refactor main

--- src/fr/iut/adaugustaperangusta/Main.java
+++ src/fr/iut/adaugustaperangusta/Main.java
@@ -10,13 +10,15 @@ import fr.iut.adaugustaperangusta.utils.CreateMap;
  * @version 1.1.0
  */
 public class Main {
+	private static final String DEFAULT_MAP = "Test.txt";
+	private static final boolean USE_GUI = true;
+	private static final boolean NO_USE_GUI = false;
+
 	/**
 	 * Entry point of the program.
 	 */
 	public static void main(String[] args) {
-		// Game game = new Game(CreateMap.importFromFile("Test.txt"),false);
-		// game.play();
-		Game game = new Game(CreateMap.importFromFile("Test.txt"), true);
+		Game game = new Game(CreateMap.importFromFile(DEFAULT_MAP), USE_GUI);
 		game.play();
 	}
 
